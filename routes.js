359d6aa317c83b9ac6a692d49edbe5f4a38e7996
Damian <d.strzalka94@wp.pl>
2024-02-12 22:05:38 +0100

    add typescript

--- routes.js
+++
@@ -1,2 +0,0 @@
-AddPolyline('}hmoHa}itBb{Dia@~PhRwSfdDuH|gAnVbqDdNfjBhYtcAxxBxcB_QxeAqDd_@xSh{A', '#ff0000', 'opis1')
-AddPolyline('w{loHkijtBj@{bHfk@_sCvfB{yE', '#00ff00', 'opis2')
---
+++ useful-links.md
@@ -0,0 +1,2 @@
+https://www.npmjs.com/package/sqlite
+https://bobbyhadz.com/blog/typescript-write-to-a-file
